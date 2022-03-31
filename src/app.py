eb7bce349330d38c042de867d759cfe009c2c4a0
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-03-31 14:53:28 +0300

    added template and initial functions for registering

--- src/app.py
+++ src/app.py
@@ -1,7 +1,19 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/register")
+def new_user():
+    return render_template("register.html")
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    password2 = request.form["password2"]
+    # check validity here
+    pass
