a6ef0f21ac044e464df997fe2db6692cc5ec7f21
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2022-04-01 16:29:56 +0300

    istunto lisätty 2

--- src/db.py
+++ src/db.py
@@ -2,7 +2,7 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///jovajova"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user" #userin tilalle oma
 db = SQLAlchemy(app)
 
 
