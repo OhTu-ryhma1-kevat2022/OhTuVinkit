b17d3938f0269e7d8d8710ce2331dbf3b5166f13
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-04-03 01:05:50 +0300

    refactoring

--- src/repositories/tips_repository.py
+++ src/repositories/tips_repository.py
@@ -1,17 +1,21 @@
 from db import db
 from flask import session
 
-def get_list():
-    sql = "SELECT tittle, link FROM tips"
-    result = db.session.execute(sql)
-    return result.fetchall()
+class TipsRepository:
 
-def add(tittle,link):
+    def get_list(self):
+        sql = "SELECT tittle, link FROM tips"
+        result = db.session.execute(sql)
+        return result.fetchall()
 
-    user_id = session.get("user_id",0)
-    if user_id == 0:
-        return False
-    sql = "INSERT INTO tips (user_id, tittle,link) VALUES (:user_id, :tittle, :link)"
-    db.session.execute(sql, {"user_id":user_id, "tittle":tittle, "link":link})
-    db.session.commit()
-    return True
+    def add(self, tittle, link):
+
+        user_id = session.get("user_id",0)
+        if user_id == 0:
+            return False
+        sql = "INSERT INTO tips (user_id, tittle,link) VALUES (:user_id, :tittle, :link)"
+        db.session.execute(sql, {"user_id":user_id, "tittle":tittle, "link":link})
+        db.session.commit()
+        return True
+
+tips_repository = TipsRepository()
--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,3 +1,7 @@
+from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import session
+
 class UserRepository:
     def __init__(self):
         self._users = []
@@ -32,5 +36,40 @@ class UserRepository:
 
     def delete_all(self):
         self._users = []
+
+    def login(self, username, password):
+
+        sql = "SELECT password, id FROM users WHERE username=:username"
+        result = db.session.execute(sql, {"username":username})
+        user = result.fetchone()
+        if user == None:
+            return False
+        else:
+            if check_password_hash(user[0],password):
+
+                session['user_id'] = user.id
+                session['username'] = username
+                session['logged_in'] = True
+
+                return True
+            else:
+                return False
+
+    def logout(self):
+        del session["user_id"]
+        session['logged_in'] = False
+
+
+    def register(self, username, password):
+        hash_value = generate_password_hash(password)
+        try:
+            sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+            db.session.execute(sql, {"username":username,"password":hash_value})
+            db.session.commit()
+        except:
+            raise Exception(
+                f"User with username {username} already exists"
+            )
+        self.login(username, password)
 
 user_repository = UserRepository()
