f5376d2b88fb74389916541082f59ef23faf8f8b
Miia <miiaheiskan@gmail.com>
2022-04-05 09:08:58 +0300

    tip + some changes to tip repo

---
+++ src/entities/tip.py
@@ -0,0 +1,7 @@
+class ReadingTip:
+    def __init__(self, user_id, title, link):  # description
+        self.user_id = user_id
+        self.title = title
+        self.link = link
+        # self.description = description
+
