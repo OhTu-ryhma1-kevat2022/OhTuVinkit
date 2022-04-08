3c6750060fe49e03ea2b3f60980b35db87e76780
Veera0742 <veera.lumioksa@gmail.com>
2022-04-08 08:54:18 +0300

    Changes to SQLAlchemy

--- src/db.py
+++ src/db.py
@@ -2,7 +2,8 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_URL
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL.replace("://", "ql://", 1)
+#app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 db = SQLAlchemy(app)
 
 
