5f1d288738bf2f31f9061e877cab3d7863dad862
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-04-11 20:09:06 +0300

    Create test file for tips repository

---
+++ src/tests/tips_repository_test.py
@@ -0,0 +1,10 @@
+import unittest
+from repositories.tips_repository import tips_repository
+
+
+class TestTipsRepository(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_list_returns_all_created_tips(self):
+        pass
